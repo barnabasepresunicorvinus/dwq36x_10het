93709fb4c2426fd0baff77bf323b3361d2edc46e
Barnabás Epres <barnabas.epres@stud.uni-corvinus.hu>
2023-11-21 09:07:34 +0100

    Kész Pascal

---
+++ WebAPIBase/wwwroot/scripts/pascalJS.js
@@ -0,0 +1,28 @@
+﻿window.onload = function () {
+    drawPascal(10);
+}
+
+var drawPascal = function (n) {
+    var szuloElem = document.getElementById('pascal');
+    for (var sor = 0; sor < n; sor++) {
+        var ujSor = document.createElement('div');
+        ujSor.classList.add('sor');
+        szuloElem.appendChild(ujSor);
+
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            var ujOszlop = document.createElement('div');
+            ujOszlop.classList.add('elem');
+            var ertek = calculatePascalValue(sor, oszlop);
+            ujOszlop.innerHTML = ertek;
+            ujSor.appendChild(ujOszlop);
+        }
+    }
+}
+
+var calculatePascalValue = function (row, col) {
+    if (col === 0 || col === row) {
+        return 1;
+    } else {
+        return calculatePascalValue(row - 1, col - 1) + calculatePascalValue(row - 1, col);
+    }
+}
