dbd3d768682bdd0db9b00a7b40f0031cdd6f4c6c
Barnabás Epres <barnabas.epres@stud.uni-corvinus.hu>
2023-11-28 09:14:58 +0100

    JS code added

--- WebAPIBase/Controllers/HajoController.cs
+++ WebAPIBase/Controllers/HajoController.cs
@@ -4,19 +4,21 @@ using WebAPIBase.Models;
 
 namespace WebAPIBase.Controllers
 {
-    //[Route("api/[controller]")]
     [ApiController]
-    public class HajoController : ControllerBase
+    public class BoatController : ControllerBase
     {
         [HttpGet]
-        [Route("questions/all")]
-        public IActionResult MindegyHogyHivjak()
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
         {
             HajosContext context = new HajosContext();
-            var kérdések = from x in context.Questions select x.Question1;
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
 
-            return Ok(kérdések);
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
         }
-
     }
 }
